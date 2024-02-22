d33a0711435c2523c5ab7be562152f4367fd7608
LuizHenryBR <siqueiralh7768@hotmail.com>
2024-02-22 02:39:51 -0300

    Project created

---
+++ src/test/java/com/exampleltda/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleltda.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
